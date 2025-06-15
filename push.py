0e9123b13c881a008f874a98e8c7bac0bd384d32
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-06-15 23:09:24 +0200

    Update push.py

--- push.py
+++ push.py
@@ -1,20 +1,23 @@
-CREATE QUERY GetInfraFromApp(STRING appAUID) FOR GRAPH InfraGraph {
-  
+CREATE QUERY GetInfraForProduction() FOR GRAPH UKG_V2 {
+
   SetAccum<VERTEX> @@infraSet;
 
-  Start = SELECT a
-          FROM application:a
-          WHERE a.AUID == appAUID;
+  // Étape 1 : application fonctionnelle
+  Start = SELECT a FROM Application:a
+          WHERE a.auid == "AP85343";
 
-  // Parcours vers les serveurs
-  ToServeurs = SELECT s
-               FROM Start:a - (use:e) -> serveur:s
-               ACCUM @@infraSet += s;
+  // Étape 2 : applications techniques en environnement "Production"
+  AppTech = SELECT at FROM Start:a - (USES:e) -> Application:at
+            WHERE at.environment == "Production";
 
-  // Parcours vers les clusters
-  ToClusters = SELECT c
-               FROM Start:a - (use:e) -> cluster:c
+  // Étape 3 : serveurs liés
+  ToServers = SELECT s FROM AppTech:at - (USES:e) -> Server:s
+              ACCUM @@infraSet += s;
+
+  // Étape 4 : clusters liés
+  ToClusters = SELECT c FROM AppTech:at - (USES:e) -> Cluster:c
                ACCUM @@infraSet += c;
 
+  // Résultat final
   PRINT @@infraSet;
 }
