f48e4f95fd34ce30fed9aba1744ff7aa2e6a9d57
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-06-15 23:17:39 +0200

    Update push.py

--- push.py
+++ push.py
@@ -1,6 +1,7 @@
-CREATE QUERY GetInfraForProduction() FOR GRAPH UKG_V2 {
+CREATE QUERY GetInfraAndChangesForProduction() FOR GRAPH UKG_V2 {
 
   SetAccum<VERTEX> @@infraSet;
+  SetAccum<VERTEX> @@changeSet;
 
   // Étape 1 : application fonctionnelle
   Start = SELECT a FROM Application:a
@@ -18,6 +19,15 @@ CREATE QUERY GetInfraForProduction() FOR GRAPH UKG_V2 {
   ToClusters = SELECT c FROM AppTech:at - (USES:e) -> Cluster:c
                ACCUM @@infraSet += c;
 
+  // Étape 5 : changes depuis serveurs
+  ServerChanges = SELECT ch FROM ToServers:s - (IMPACTS:e) -> Change:ch
+                  ACCUM @@changeSet += ch;
+
+  // Étape 6 : changes depuis clusters
+  ClusterChanges = SELECT ch FROM ToClusters:c - (IMPACTS:e) -> Change:ch
+                   ACCUM @@changeSet += ch;
+
   // Résultat final
   PRINT @@infraSet;
+  PRINT @@changeSet;
 }
