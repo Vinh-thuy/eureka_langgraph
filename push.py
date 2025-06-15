70e5653d3822e0ce7255c1a2de0061f865cc9a93
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-06-16 00:16:04 +0200

    Update push.py

--- push.py
+++ push.py
@@ -1,4 +1,10 @@
-MATCH (app:Application {auid: "AP85343"})-[:USES]->(tech:Application {environment: "Production"})
-OPTIONAL MATCH (tech)-[:USES]->(s:Server)-[:IMPACTS]->(c1:Change)
-OPTIONAL MATCH (tech)-[:USES]->(cl:Cluster)-[:IMPACTS]->(c2:Change)
-RETURN DISTINCT app, tech, s, c1, cl, c2
+MATCH (a:Application {auid: "AP85343"})-[:USES]->(at:Application)
+WHERE at.environment = "Production"
+
+OPTIONAL MATCH (at)-[:USES]->(s:Server)
+OPTIONAL MATCH (s)-[:IMPACTS]->(c1:Change)
+
+OPTIONAL MATCH (at)-[:USES]->(cl:Cluster)
+OPTIONAL MATCH (cl)-[:IMPACTS]->(c2:Change)
+
+RETURN DISTINCT a, at, s, c1, cl, c2
