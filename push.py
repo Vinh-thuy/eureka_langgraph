ed529964759ed482a1468c55251db4b3abeb2f10
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-06-16 00:08:09 +0200

    Update push.py

--- push.py
+++ push.py
@@ -3,25 +3,25 @@ CREATE QUERY GetInfraForProduction() FOR GRAPH UKG_V2 {
   SetAccum<VERTEX> @@infraSet;
   SetAccum<VERTEX> @@changeSet;
 
-  // Étape 1 : serveurs liés et leurs changes
-  ServerChanges = SELECT ch 
-    FROM Application:a -(USES:e1)-> Application:at
-                   -(USES:e2)-> Server:s
-                   -(IMPACTS:e3)-> Change:ch
+  // Étape 1 : serveurs liés aux apps techniques
+  SELECT ch FROM Application:a
+    -(USES)-> Application:at
+    -(USES)-> Server:s
+    -(IMPACTS)-> Change:ch
     WHERE a.auid == "AP85343" AND at.environment == "Production"
-    ACCUM @@infraSet += s, @@changeSet += ch;
-  PRINT ServerChanges;
+    ACCUM @@infraSet += s,
+          @@changeSet += ch;
 
-  // Étape 2 : clusters liés et leurs changes
-  ClusterChanges = SELECT ch 
-    FROM Application:a -(USES:e1)-> Application:at
-                   -(USES:e2)-> Cluster:c
-                   -(IMPACTS:e3)-> Change:ch
+  // Étape 2 : clusters liés aux apps techniques
+  SELECT ch FROM Application:a
+    -(USES)-> Application:at
+    -(USES)-> Cluster:c
+    -(IMPACTS)-> Change:ch
     WHERE a.auid == "AP85343" AND at.environment == "Production"
-    ACCUM @@infraSet += c, @@changeSet += ch;
-  PRINT ClusterChanges;
+    ACCUM @@infraSet += c,
+          @@changeSet += ch;
 
-  // Résultat global
+  // Résultat final
   PRINT @@infraSet;
   PRINT @@changeSet;
 }
