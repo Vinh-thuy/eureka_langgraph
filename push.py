cb6af3cc45f7ee7ad85ca21fb75bc8495db6a495
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-06-15 23:37:08 +0200

    Update push.py

--- push.py
+++ push.py
@@ -1,30 +1,23 @@
 CREATE QUERY GetInfraFromApp() FOR GRAPH UKG_V2 {
-
   SetAccum<VERTEX> @@infraSet;
   SetAccum<VERTEX> @@changeSet;
 
-  // Parcours servers + changes en un seul SELECT
+  // 1) Serveurs + Changes
   ServerPaths = SELECT s
-                FROM Application:a
-                     -(USES:e1)-Application:at
-                     -(USES:e2)-Server:s
-                     -(IMPACTS:e3)-Change:ch
-                WHERE a.auid == "AP85343"
-                  AND at.environment == "Production"
-                ACCUM @@infraSet += s,
-                      @@changeSet += ch;
+    FROM Application:a -(USES:e1)-> Application:at -(USES:e2)-> Server:s -(IMPACTS:e3)-> Change:ch
+    WHERE a.auid == "AP85343"
+      AND at.environment == "Production"
+    ACCUM @@infraSet += s,
+          @@changeSet += ch;
   PRINT ServerPaths;
 
-  // Parcours clusters + changes en un seul SELECT
+  // 2) Clusters + Changes
   ClusterPaths = SELECT c
-                 FROM Application:a
-                      -(USES:e1)-Application:at
-                      -(USES:e2)-Cluster:c
-                      -(IMPACTS:e3)-Change:ch
-                 WHERE a.auid == "AP85343"
-                   AND at.environment == "Production"
-                 ACCUM @@infraSet += c,
-                       @@changeSet += ch;
+    FROM Application:a -(USES:e1)-> Application:at -(USES:e2)-> Cluster:c -(IMPACTS:e3)-> Change:ch
+    WHERE a.auid == "AP85343"
+      AND at.environment == "Production"
+    ACCUM @@infraSet += c,
+          @@changeSet += ch;
   PRINT ClusterPaths;
 
   // Résultats finaux
