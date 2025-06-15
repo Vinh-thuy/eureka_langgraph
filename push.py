a95e117c2b11609d78c07ff4513b95e616d03ee8
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-06-16 01:00:28 +0200

    Update push.py

--- push.py
+++ push.py
@@ -1,16 +1,11 @@
 MATCH (a:Application {auid: "AP85343"})-[:USES]->(at:Application)
 WHERE at.environment = "Production"
-
 MATCH (at)-[:USES]->(cl:Cluster)
-MATCH (cl)-[:IMPACTS]->(c:Change)
-
+OPTIONAL MATCH (c:Change)-[:IMPACTS]->(cl)
+OPTIONAL MATCH (i:Incident)-[:IMPACTS]->(cl)
 RETURN
   a.auid AS app_code,
   at.environment AS env,
-  at.name AS tech_app_name,
   cl.name AS cluster_name,
-  cl.region AS cluster_region,
   c.change_id AS change_id,
-  c.type AS change_type,
-  c.status AS change_status,
-  c.date AS change_date
+  i.incident_id AS incident_id
