bb5a95de722e6556af5e4df65acfd511fa1d7393
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-06-15 23:40:19 +0200

    Update push.py

--- push.py
+++ push.py
@@ -4,24 +4,21 @@ CREATE QUERY GetInfraFromApp(STRING appId) FOR GRAPH UKG_V2 {
 
   // 1) Serveurs + Changes
   ServerPaths = SELECT s
-    FROM Application:a -(USES:e1)-> Application:at 
-           -(USES:e2)-> Server:s 
-           -(IMPACTS:e3)-> Change:ch
+    FROM Application:a -(USES:e1)-> Application:at,
+         Application:at -(USES:e2)-> Server:s,
+         Server:s -(IMPACTS:e3)-> Change:ch
     WHERE a.auid == appId
       AND at.environment == "Production"
     ACCUM {
       @@infraSet += s,
       @@changeSet += ch
-    }
-    POST-ACCUM
-      // Logique supplémentaire si nécessaire
-    ;
+    };
 
   // 2) Clusters + Changes
   ClusterPaths = SELECT c
-    FROM Application:a -(USES:e1)-> Application:at 
-           -(USES:e2)-> Cluster:c 
-           -(IMPACTS:e3)-> Change:ch
+    FROM Application:a -(USES:e1)-> Application:at,
+         Application:at -(USES:e2)-> Cluster:c,
+         Cluster:c -(IMPACTS:e3)-> Change:ch
     WHERE a.auid == appId
       AND at.environment == "Production"
     ACCUM {
