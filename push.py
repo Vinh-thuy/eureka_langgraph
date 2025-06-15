a07ba46bb806b2a4ea8bdcf16567303c587bec94
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-06-15 23:38:38 +0200

    Update push.py

--- push.py
+++ push.py
@@ -1,24 +1,33 @@
-CREATE QUERY GetInfraFromApp() FOR GRAPH UKG_V2 {
+CREATE QUERY GetInfraFromApp(STRING appId) FOR GRAPH UKG_V2 {
   SetAccum<VERTEX> @@infraSet;
   SetAccum<VERTEX> @@changeSet;
 
   // 1) Serveurs + Changes
   ServerPaths = SELECT s
-    FROM Application:a -(USES:e1)-> Application:at -(USES:e2)-> Server:s -(IMPACTS:e3)-> Change:ch
-    WHERE a.auid == "AP85343"
+    FROM Application:a -(USES:e1)-> Application:at 
+           -(USES:e2)-> Server:s 
+           -(IMPACTS:e3)-> Change:ch
+    WHERE a.auid == appId
       AND at.environment == "Production"
-    ACCUM @@infraSet += s,
-          @@changeSet += ch;
-  PRINT ServerPaths;
+    ACCUM {
+      @@infraSet += s,
+      @@changeSet += ch
+    }
+    POST-ACCUM
+      // Logique supplémentaire si nécessaire
+    ;
 
   // 2) Clusters + Changes
   ClusterPaths = SELECT c
-    FROM Application:a -(USES:e1)-> Application:at -(USES:e2)-> Cluster:c -(IMPACTS:e3)-> Change:ch
-    WHERE a.auid == "AP85343"
+    FROM Application:a -(USES:e1)-> Application:at 
+           -(USES:e2)-> Cluster:c 
+           -(IMPACTS:e3)-> Change:ch
+    WHERE a.auid == appId
       AND at.environment == "Production"
-    ACCUM @@infraSet += c,
-          @@changeSet += ch;
-  PRINT ClusterPaths;
+    ACCUM {
+      @@infraSet += c,
+      @@changeSet += ch
+    };
 
   // Résultats finaux
   PRINT @@infraSet;
