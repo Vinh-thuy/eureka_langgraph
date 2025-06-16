1a6de0389eda00aef345f0bf24f72fabbbafc072
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-06-16 08:38:47 +0200

    Update push.py

--- push.py
+++ push.py
@@ -1,16 +1,18 @@
-
-
 CREATE QUERY GetInfraPaths(STRING auid, STRING env) FOR GRAPH UKG_V2 {
-  // Sélection de tous les chemins Application -> Application(env) -> Cluster
-  pathResults =
-    SELECT a, a2, c
-    FROM Application:a -(USES:e1)-> Application:a2 -(USES:e2)-> Cluster:c
-    WHERE a.auid == auid
-      AND a2.environment == env;
+  // 1. Handle de tous les vertices Application
+  appVertices = { Application.* };
 
-  // Affichage de chaque chemin complet (startApp, envApp, cluster)
-  PRINT pathResults;
+  // 2. Sélection de tous les chemins Application->Application(env)->Cluster
+  paths =
+    SELECT a, b, c
+    FROM appVertices:a -(USES:e1)-> appVertices:b -(USES:e2)-> Cluster:c
+    WHERE a.auid == auid
+      AND b.environment == env;
+
+  // 3. Affichage des chemins complets
+  PRINT paths;
 }
+
 
 
 
