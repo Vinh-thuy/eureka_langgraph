c4e14e7b7681c1a3ec6cae883b41d5f43747552b
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-06-16 08:10:43 +0200

    Update push.py

--- push.py
+++ push.py
@@ -2,18 +2,16 @@ CREATE QUERY GetInfraFromApp() FOR GRAPH UKG_V2 {
   SetAccum<VERTEX> @@infraSet;
   SetAccum<VERTEX> @@changeSet;
 
-  // 1) Tout faire en une seule requête
-  Result = SELECT c
-    FROM (a:Application)-[:USES]->(at:Application),
-         (at)-[:USES]->(c:Cluster),
-         (c)-[:IMPACTS]->(ch:Change)
+  Result = SELECT ch
+    FROM Application:a - (USES) -> Application:at
+                      - (USES) -> Cluster:c
+                      - (IMPACTS) -> Change:ch
     WHERE a.auid == "AP85343"
       AND at.environment == "Production"
-    ACCUM 
+    ACCUM
       @@infraSet += c,
       @@changeSet += ch;
 
-  // Résultats finaux
   PRINT @@infraSet;
   PRINT @@changeSet;
 }
