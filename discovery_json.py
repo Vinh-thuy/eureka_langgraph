612ad4359a6ea5e3c2e57bd01ca9477d50b09412
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-07-15 23:47:19 +0200

    Update discovery_json.py

--- discovery_json.py
+++ discovery_json.py
@@ -1,31 +1,103 @@
 {
   "clusters": [
-    { "id": "c1", "name": "prod-cluster", "namespaces": ["ns1", "ns2"], "nodes": ["n1", "n2"] }
+    {
+      "name": "prod-cluster",
+      "namespaces": ["default", "monitoring"],
+      "nodes": ["node-1", "node-2"]
+    }
   ],
   "namespaces": [
-    { "id": "ns1", "name": "default", "parent": "c1", "deployments": ["d1", "d2"] },
-    { "id": "ns2", "name": "monitoring", "parent": "c1", "deployments": ["d3"] }
+    {
+      "name": "default",
+      "parent": "prod-cluster",
+      "deployments": ["web-app", "api-service"]
+    },
+    {
+      "name": "monitoring",
+      "parent": "prod-cluster",
+      "deployments": ["prometheus"]
+    }
   ],
   "nodes": [
-    { "id": "n1", "name": "node-1", "parent": "c1", "pods": ["p1", "p2"] },
-    { "id": "n2", "name": "node-2", "parent": "c1", "pods": ["p3", "p4"] }
+    {
+      "name": "node-1",
+      "parent": "prod-cluster",
+      "pods": ["web-app-pod-1", "web-app-pod-2"]
+    },
+    {
+      "name": "node-2",
+      "parent": "prod-cluster",
+      "pods": ["api-pod-1", "prometheus-pod-1"]
+    }
   ],
   "deployments": [
-    { "id": "d1", "name": "web-app", "parent": "ns1", "pods": ["p1", "p2"] },
-    { "id": "d2", "name": "api-service", "parent": "ns1", "pods": ["p3"] },
-    { "id": "d3", "name": "prometheus", "parent": "ns2", "pods": ["p4"] }
+    {
+      "name": "web-app",
+      "parent": "default",
+      "pods": ["web-app-pod-1", "web-app-pod-2"]
+    },
+    {
+      "name": "api-service",
+      "parent": "default",
+      "pods": ["api-pod-1"]
+    },
+    {
+      "name": "prometheus",
+      "parent": "monitoring",
+      "pods": ["prometheus-pod-1"]
+    }
   ],
   "pods": [
-    { "id": "p1", "name": "web-app-pod-1", "parent_deployment": "d1", "parent_node": "n1", "containers": ["c1a", "c1b"] },
-    { "id": "p2", "name": "web-app-pod-2", "parent_deployment": "d1", "parent_node": "n1", "containers": ["c2a"] },
-    { "id": "p3", "name": "api-pod-1", "parent_deployment": "d2", "parent_node": "n2", "containers": ["c3a"] },
-    { "id": "p4", "name": "prometheus-pod-1", "parent_deployment": "d3", "parent_node": "n2", "containers": ["c4a"] }
+    {
+      "name": "web-app-pod-1",
+      "parent_deployment": "web-app",
+      "parent_node": "node-1",
+      "containers": ["nginx-1", "sidecar-logger-1"]
+    },
+    {
+      "name": "web-app-pod-2",
+      "parent_deployment": "web-app",
+      "parent_node": "node-1",
+      "containers": ["nginx-2"]
+    },
+    {
+      "name": "api-pod-1",
+      "parent_deployment": "api-service",
+      "parent_node": "node-2",
+      "containers": ["api-1"]
+    },
+    {
+      "name": "prometheus-pod-1",
+      "parent_deployment": "prometheus",
+      "parent_node": "node-2",
+      "containers": ["prometheus-1"]
+    }
   ],
   "containers": [
-    { "id": "c1a", "name": "nginx", "parent": "p1", "image": "nginx:1.21" },
-    { "id": "c1b", "name": "sidecar-logger", "parent": "p1", "image": "logger:v2" },
-    { "id": "c2a", "name": "nginx", "parent": "p2", "image": "nginx:1.21" },
-    { "id": "c3a", "name": "api", "parent": "p3", "image": "api-service:v3" },
-    { "id": "c4a", "name": "prometheus", "parent": "p4", "image": "prom/prometheus:v2.31" }
+    {
+      "name": "nginx-1",
+      "parent": "web-app-pod-1",
+      "image": "nginx:1.21"
+    },
+    {
+      "name": "sidecar-logger-1",
+      "parent": "web-app-pod-1",
+      "image": "logger:v2"
+    },
+    {
+      "name": "nginx-2",
+      "parent": "web-app-pod-2",
+      "image": "nginx:1.21"
+    },
+    {
+      "name": "api-1",
+      "parent": "api-pod-1",
+      "image": "api-service:v3"
+    },
+    {
+      "name": "prometheus-1",
+      "parent": "prometheus-pod-1",
+      "image": "prom/prometheus:v2.31"
+    }
   ]
 }
